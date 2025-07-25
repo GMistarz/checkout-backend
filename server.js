5e558e561c6f92dc569fc2a4cf9662fac5ba7e64
Gregory Mistarz <gmistarz@csmfg.local>
2025-07-25 08:26:23 -0400

    Update server.js for email attachment layout

--- server.js
+++ server.js
@@ -1070,7 +1070,7 @@ function generateOrderHtmlEmail(orderData) {
 
             <hr style="border: none; border-top: 1px solid #dcdcdc; margin: 5px 0 10px 0;">
 
-            <p style="font-size: 16px; font-weight: bold; color: #000000; margin: 0 0 15px 0;"><strong>PO#:</strong> ${orderData.poNumber}</p>
+            <p style="font-size: 18px; font-weight: bold; color: #000000; margin: 0 0 15px 0;"><strong>PO#:</strong> ${orderData.poNumber}</p>
 
             <table style="width: 100%; border-collapse: collapse; margin-bottom: 20px;">
                 <tr>
