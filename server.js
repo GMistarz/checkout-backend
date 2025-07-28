50095fed29ee7e9bfed132aef6d9f27aa63b6618
Gregory Mistarz <gmistarz@csmfg.local>
2025-07-28 14:40:15 -0400

    Update server.js with UPS, FedEx and DHL stamps

--- server.js
+++ server.js
@@ -1112,11 +1112,11 @@ function generateOrderHtmlEmail(orderData) {
                    shippingMethodLower.includes("overnight");
 
     // RUSH image HTML - positioned absolutely over the content
-//    const rushImageHtml = isRush ? `
-//        <div style="position: absolute; top: -5px; right: 20px; z-index: 100;">
-//            <img src="https://www.chicagostainless.com/graphics/stamps/rush.png" alt="RUSH" style="max-width: 170px; height: auto; display: block; opacity: 0.5;">
-//        </div>
-//    ` : ''; // Empty if not rush
+    const rushImageHtml = isRush ? `
+        <div style="position: absolute; top: -5px; right: 20px; z-index: 100;">
+            <img src="https://www.chicagostainless.com/graphics/stamps/rush.png" alt="RUSH" style="max-width: 170px; height: auto; display: block; opacity: 0.5;">
+        </div>
+    ` : ''; // Empty if not rush
 
     // Determine carrier logo
     let carrierLogoHtml = '';
